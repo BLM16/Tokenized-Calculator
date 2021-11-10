05c93ea62285696352317e021f67ed8e54024d12
Bradley Myers <myers.bradley@hotmail.com>
2021-11-10 11:25:52 -0500

    Added example program

---
+++ Examples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter your equation: ");
+                    var eq = Console.ReadLine();
+            
+                    var calculator = new Calculator.Calculator();
+                    var res = calculator.Calculate(eq);
+
+                    Console.WriteLine($"Result: {res}\n");
+                }
+                catch (Calculator.MathSyntaxException e)
+                {
+                    Console.WriteLine(e + "\n");
+                }
+            }
+        }
+    }
+}
