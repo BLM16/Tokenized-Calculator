31e6f32e6406a74e81007997477bdefef78206c3
Bradley Myers <myers.bradley@hotmail.com>
2022-05-05 23:35:40 -0400

    Fixed scientific notation formatting issue from functions

--- Calculator/Models/Function.cs
+++ Calculator/Models/Function.cs
@@ -29,106 +29,118 @@ public struct Function
 public static class DefaultFunctions
 {
     /// <summary>
+    /// Formats doubles after functions have been evaluated
+    /// </summary>
+    /// <remarks>
+    /// This should be lossless for the datatype, remove trailing zeros,
+    /// and avoid scientific notation which interferes in calculations when
+    /// E is treated as a constant and not as scientific notation.
+    /// </remarks>
+    /// <returns>The double as a lossless string without scientific notation or trailing zeros</returns>
+    private static string FormatAsString(this double d)
+        => d.ToString("F339").TrimEnd('0').TrimEnd('.');
+
+    /// <summary>
     /// The absolute function
     /// </summary>
     public static Function Abs
-        => new((double val) => Math.Abs(val).ToString(), "abs");
+        => new((double val) => Math.Abs(val).FormatAsString(), "abs");
 
     /// <summary>
     /// The square root function
     /// </summary>
     public static Function Sqrt
-        => new((double val) => Math.Sqrt(val).ToString(), "sqrt", "root", "√");
+        => new((double val) => Math.Sqrt(val).FormatAsString(), "sqrt", "root", "√");
 
     /// <summary>
     /// The cubed root function
     /// </summary>
     public static Function Cbrt
-        => new((double val) => Math.Cbrt(val).ToString(), "cbrt", "sqrt3", "root3");
+        => new((double val) => Math.Cbrt(val).FormatAsString(), "cbrt", "sqrt3", "root3");
 
     /// <summary>
     /// The sine function with a parameter in radians
     /// </summary>
     public static Function Sin
-        => new((double val) => Math.Sin(val).ToString(), "sin");
+        => new((double val) => Math.Sin(val).FormatAsString(), "sin");
 
     /// <summary>
     /// The cosine function with a parameter in radians
     /// </summary>
     public static Function Cos
-        => new((double val) => Math.Cos(val).ToString(), "cos");
+        => new((double val) => Math.Cos(val).FormatAsString(), "cos");
 
     /// <summary>
     /// The tangent function with a parameter in radians
     /// </summary>
     public static Function Tan
-        => new((double val) => Math.Tan(val).ToString(), "tan");
+        => new((double val) => Math.Tan(val).FormatAsString(), "tan");
 
     /// <summary>
     /// The inverse sine function
     /// </summary>
     public static Function Asin
-        => new((double val) => Math.Asin(val).ToString(), "asin", "arcsin", "sin^-1", "sin^(-1)");
+        => new((double val) => Math.Asin(val).FormatAsString(), "asin", "arcsin", "sin^-1", "sin^(-1)");
 
     /// <summary>
     /// The inverse cosine function
     /// </summary>
     public static Function Acos
-        => new((double val) => Math.Acos(val).ToString(), "acos", "arccos", "cos^-1", "cos^(-1)");
+        => new((double val) => Math.Acos(val).FormatAsString(), "acos", "arccos", "cos^-1", "cos^(-1)");
 
     /// <summary>
     /// The inverse tangent function
     /// </summary>
     public static Function Atan
-        => new((double val) => Math.Atan(val).ToString(), "atan", "arctan", "tan^-1", "tan^(-1)");
+        => new((double val) => Math.Atan(val).FormatAsString(), "atan", "arctan", "tan^-1", "tan^(-1)");
 
     /// <summary>
     /// The hyperbolic sine function
     /// </summary>
     public static Function Sinh
-        => new((double val) => Math.Sinh(val).ToString(), "sinh");
+        => new((double val) => Math.Sinh(val).FormatAsString(), "sinh");
 
     /// <summary>
     /// The hyperbolic cosine function
     /// </summary>
     public static Function Cosh
-        => new((double val) => Math.Cosh(val).ToString(), "cosh");
+        => new((double val) => Math.Cosh(val).FormatAsString(), "cosh");
 
     /// <summary>
     /// The hyperbolic tangent function
     /// </summary>
     public static Function Tanh
-        => new((double val) => Math.Tanh(val).ToString(), "tanh");
+        => new((double val) => Math.Tanh(val).FormatAsString(), "tanh");
 
     /// <summary>
     /// The inverse hyperbolic sine function
     /// </summary>
     public static Function Asinh
-        => new((double val) => Math.Asinh(val).ToString(), "asinh", "arcsinh", "sinh^-1", "sinh^(-1)");
+        => new((double val) => Math.Asinh(val).FormatAsString(), "asinh", "arcsinh", "sinh^-1", "sinh^(-1)");
 
     /// <summary>
     /// The inverse hyperbolic cosine function
     /// </summary>
     public static Function Acosh
-        => new((double val) => Math.Acosh(val).ToString(), "acosh", "arccosh", "cosh^-1", "cosh^(-1)");
+        => new((double val) => Math.Acosh(val).FormatAsString(), "acosh", "arccosh", "cosh^-1", "cosh^(-1)");
 
     /// <summary>
     /// The inverse hyperbolic tangent function
     /// </summary>
     public static Function Atanh
-        => new((double val) => Math.Atanh(val).ToString(), "atanh", "arctanh", "tanh^-1", "tanh^(-1)");
+        => new((double val) => Math.Atanh(val).FormatAsString(), "atanh", "arctanh", "tanh^-1", "tanh^(-1)");
 
     /// <summary>
     /// The ceiling function
     /// </summary>
     public static Function Ceil
-        => new((double val) => Math.Ceiling(val).ToString(), "ceil");
+        => new((double val) => Math.Ceiling(val).FormatAsString(), "ceil");
 
     /// <summary>
     /// The floor function
     /// </summary>
     public static Function Floor
-        => new((double val) => Math.Floor(val).ToString(), "floor");
+        => new((double val) => Math.Floor(val).FormatAsString(), "floor");
 
     /// <summary>
     /// The sign function
@@ -140,29 +152,29 @@ public static class DefaultFunctions
     /// The logarithm function of base e
     /// </summary>
     public static Function Log
-        => new((double val) => Math.Log(val).ToString(), "loge", "log_e", "ln");
+        => new((double val) => Math.Log(val).FormatAsString(), "loge", "log_e", "ln");
 
     /// <summary>
     /// The logarithm function of base 2
     /// </summary>
     public static Function Log2
-        => new((double val) => Math.Log2(val).ToString(), "log2", "log_2");
+        => new((double val) => Math.Log2(val).FormatAsString(), "log2", "log_2");
 
     /// <summary>
     /// The logarithm function of base 10
     /// </summary>
     public static Function Log10
-        => new((double val) => Math.Log10(val).ToString(), "log10", "log_10", "log");
+        => new((double val) => Math.Log10(val).FormatAsString(), "log10", "log_10", "log");
 
     /// <summary>
     /// The degree function converts radians to degrees
     /// </summary>
     public static Function Deg
-        => new((double val) => (val * 180 / Math.PI).ToString(), "deg");
+        => new((double val) => (val * 180 / Math.PI).FormatAsString(), "deg");
 
     /// <summary>
     /// The radian function converts degrees to radians
     /// </summary>
     public static Function Rad
-        => new((double val) => (val * Math.PI / 180).ToString(), "rad");
+        => new((double val) => (val * Math.PI / 180).FormatAsString(), "rad");
 }
